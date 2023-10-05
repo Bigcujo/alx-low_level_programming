010ccf82af2f7fba8818ffd70cf1819b9a13a7bd
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-05 23:19:25 +0100

    hellooooo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  *
  * Return: always (0)
  */
-int main() {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
