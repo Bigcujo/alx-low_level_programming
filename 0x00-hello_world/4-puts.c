d41a8478724c47790c50c4ddbf9174ce14e329c0
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-05 21:18:57 +0100

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-/*
- * main
+/**
+ * main - entry point into the code
  *
- * return: always 
+ * Return: always (0)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
