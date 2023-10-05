4d007c6f6f2a022476e99ccb8feb4038f4aa707a
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-05 21:25:21 +0100

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry into this code block
+ *
+ * printf - this will be used to print the text on the terminal
+ *
+ * Return: awlays (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
