8cd3c88cecb6c0aa09ca8e20a75e3af1bc7a023f
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-07 14:56:10 +0100

    test 1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - entry into the code block
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	       printf("%d\n", a);
+return (0);
+}
