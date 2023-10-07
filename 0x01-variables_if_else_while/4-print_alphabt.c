989c3f248d8daf02c8ef970ef3eaf1470115476f
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-07 14:38:38 +0100

    hekd

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - entry into the code block
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+        char letters;
+
+        for (letters = 'a'; letters <= 'z'; letters++)
+                if ( letters != 'q' && letters != 'e') 
+		putchar(letters);
+	putchar('\n');
+	return (0);
+}
