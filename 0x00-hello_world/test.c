5310d31d84221f9689abbdd9f9724a17c83cf5aa
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-10 05:32:48 +0100

    hello

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+	char t;
+	for(int d = 0; d < 3;d++)
+	{
+		//printf("%d",d);
+		for(int s =0; s < 5 ; s++)
+		{
+			printf("%d",d);
+		printf("%d",s);
+		printf("\n");
+			//putchar('0' + d);
+		}
+	}
+	printf("\n");
+	return (0);
+}
