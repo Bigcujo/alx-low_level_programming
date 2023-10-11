50fb6f57e9b1a1db8354306ecca88a89cecfe0c4
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-12 00:10:22 +0100

    hello

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - check the code
  *
@@ -7,6 +6,7 @@
  */
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	print_alphabet();
+	_putchar('\n');
+	return (0);
 }
