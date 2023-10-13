46690993aa1ca0fa5571d0b5e919e12b2f942846
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-13 14:22:20 +0100

    hello

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - this will print number from 0 to 9 wihout 2 and 4
+ * _putchar: this print out the number 
+ */
+void print_most_numbers(void)
+{
+	int s;
+	for(s = 0; s <= 9; s++)
+	{
+		if(s != 2 && s != 4)
+		{
+			_putchar('0' + s);
+		}
+	}
+	_putchar('\n');
+}
+
