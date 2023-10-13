d45f70c2af2810606bae05aa8008cacff662c928
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-13 12:09:52 +0100

    new task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isdigit.c - this will check for a digit from 0 -9 
+ * _isdigit - this will check for a digit from 0 -9
  * @c: this is used to represent the number we are to check for
  * Return: this will return 1 is c is a digit or 0 if it's otherwise
  */
