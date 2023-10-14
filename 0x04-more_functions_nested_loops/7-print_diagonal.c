866785a5894f9cbac6f3f853e9cac6fa20530132
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-14 04:21:16 +0100

    hello

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,18 +5,25 @@
  */
 void print_diagonal(int n)
 {
-	int r;
 
-if (n > 0)
-{
-	for (r = 1; r <= n; r++)
+	if (n <= 0)
 	{
-		_putchar('\\');
-	}
 	_putchar('\n');
-}
-else if (n <= 0)
-{
-	_putchar('\n');
-}
 	}
+	else
+	{
+	int f, j;
+
+	for (f = 0; f < n; f++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (j == f)
+				_putchar('\\');
+			else if (j < f)
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	}
+}
