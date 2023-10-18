184d050ddbb8839faef86c97aac900ac90596d89
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-18 04:17:04 +0100

    hello alx

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @g: value that will be swapped
+ * @d: value that will be swapped
+ */
+void swap_int(int *g, int *d)
+{
+	int m;
+
+	m = *a;
+	*g = *d;
+	*d = m;
+}
+
