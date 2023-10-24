2301bb9ce9978b3076dc4daf8753c8ef30a65384
Bigcujo <princefrancisogbogu@gmail.com>
2023-10-24 19:34:22 +0100

    hello

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int f;
+  int arr[5];
+  int *p;
+
+  arr[2] = 1024;
+  p = &f;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("arr[2] = %d\n", arr[2]);
+  return (0);
+}
+
