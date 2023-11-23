028a79e321356533f68ba5081e97ff1587d4639d
Bigcujo <princefrancisogbogu@gmail.com>
2023-11-23 01:42:40 +0100

    hello

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *str;
-	unsigned int i;
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
+	char *string;
+	unsigned int count;
+	string = malloc(sizeof(char) * size);
+	
+	if (size == 0 || string == NULL)
+	{
 	return (NULL);
-	for (i = 0; i < size; i++)
-	str[i] = c;
-	return (str);
+	}
+	for (count = 0; count < size; count++)
+	{
+		string[count] = c;
+	}
+	return (string);
 }
