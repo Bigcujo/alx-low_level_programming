cdcab434ac41914b72a78a07111fb6f1529c1f9f
Bigcujo <princefrancisogbogu@gmail.com>
2023-11-23 01:06:04 +0100

    heloalx

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * create_array-this function will create an array of charaters
+ * @size:this will take the size of the preffered array
+ * @c:this will take the charater value we want in the array
+ * Return: this will return a charater.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *string;
+	unsigned int count = 0;
+
+	string = malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	while (count < size)
+	{
+		string[count] = c;
+		string++;
+	}
+	return (string);
+}
+
+
+
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+this is the read me file to the memory allocation task in c for alx low level programming.
