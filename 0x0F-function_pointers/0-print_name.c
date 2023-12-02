1442e8552b32dad4dd99bfaa700652083847e3f9
Bigcujo <princefrancisogbogu@gmail.com>
2023-12-02 09:56:47 -0800

    hello

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,9 +8,20 @@
  */
 void print_name(char *name, void(*f)(char *))
 {
-	if (name != NULL || f != NULL)
+    if (name != NULL) 
+	{
+	if (f != NULL) 
 	{
 	(*f)(name);
+	} 
+	else 
+	{
+	printf("%s\n", name);
 	}
-	return;
+	}
+	else
+	{
+	printf("Invalid arguments\n");
+    }
+
 }
