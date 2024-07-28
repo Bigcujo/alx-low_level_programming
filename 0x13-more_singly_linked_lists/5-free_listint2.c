ef7ab6498ca319edca9a3e00d39f2dd650127192
bigcujo <princefrancisogbogu@gmail.com>
2024-07-28 09:50:09 -0700

    free the list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - free the whole linked list
+ * @head: head pointer to the linked list
+ * Return: it return nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *temp;
+
+	if (head != NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = head->next;
+		free(temp);
+	}
+	*head = NULL;
+}
