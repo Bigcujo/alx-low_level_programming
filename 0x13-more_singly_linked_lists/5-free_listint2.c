95177c4bf9a6fe84d0a3c3d45d715d9adfedc171
bigcujo <princefrancisogbogu@gmail.com>
2024-07-28 10:36:12 -0700

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,15 @@ void free_listint2(listint_t **head)
 
 	if (head != NULL)
 	{
-		return;
-	}
+	
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
+	
 	*head = NULL;
+	}
 }
+
